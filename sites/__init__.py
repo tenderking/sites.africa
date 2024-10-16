7cba2ee1a5ae56f5947be9e3ea85effd44992638
George Mushore <gmu003@gmail.com>
2024-10-16 11:44:08 +0200

    adjusted styling and dynamic data

--- sites/__init__.py
+++ sites/__init__.py
@@ -1,4 +1,7 @@
+# ./sites/__init__.py
 from flask import Flask
+
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-from sites import views
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+from sites import views  # Import views after app is created
--- sites/views.py
+++ sites/views.py
@@ -1,7 +1,22 @@
+# ./sites/views.py
 from sites import app
 from flask import render_template
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    #return "Hello World"
-     return render_template('index.html')
+    sites = [
+        {"name": "site name 1", "url": "#", "category": "category 1"},
+        {"name": "site name 2", "url": "#", "category": "category 1"},
+        {"name": "site name 3", "url": "#", "category": "category 1"},
+        {"name": "site name 4", "url": "#", "category": "category 1"},
+        {"name": "site name 5", "url": "#", "category": "category 2"},
+        {"name": "site name 6", "url": "#", "category": "category 2"},
+        {"name": "site name 7", "url": "#", "category": "category 2"},
+        {"name": "site name 8", "url": "#", "category": "category 2"},
+    ]
+
+    # Extract unique categories
+    categories = list({site["category"] for site in sites})
+
+    return render_template("index.html", categories=categories, sites=sites)
